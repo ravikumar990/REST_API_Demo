d266c483704d8cd4c6c48f06b38edad9050a10cb
ravi <ravi@gmail.com>
2021-12-17 22:20:16 +0530

    Get request method to show hello + Name from Bridgelabz

--- src/main/java/com/example/demo/controller/RestController.java
+++ src/main/java/com/example/demo/controller/RestController.java
@@ -1,12 +1,19 @@
-package com.example.demo.controller;
+ package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+ import org.springframework.web.bind.annotation.RequestMapping;
+ import org.springframework.web.bind.annotation.RequestMethod;
+ import org.springframework.web.bind.annotation.RequestParam;
 
-@org.springframework.web.bind.annotation.RestController
-@RequestMapping("/hello")
-public class RestController {
-    @RequestMapping(value = {"", "/", "home"})
-    public String sayHello() {
-        return "Hello From Bridgelabz..";
-    }
-}
+ @org.springframework.web.bind.annotation.RestController
+ @RequestMapping("/hello")
+ public class RestController {
+     @RequestMapping(value = {"", "/", "home"})
+     public String sayHello() {
+         return "Hello From Bridgelabz..";
+     }
+
+     @RequestMapping(value = {"/query"}, method = RequestMethod.GET)
+     public String sayHello(@RequestParam(value= "name")String name){
+         return "Hello "+ name +" From Bridgelabz";
+     }
+ }
