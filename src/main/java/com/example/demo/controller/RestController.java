ac0ae322f835d7c603d33352434a64dc4ee03de9
ravi <ravi@gmail.com>
2021-12-17 22:25:10 +0530

    Get Method for passing name as Pathvariable

--- src/main/java/com/example/demo/controller/RestController.java
+++ src/main/java/com/example/demo/controller/RestController.java
@@ -1,5 +1,7 @@
  package com.example.demo.controller;
 
+ import org.springframework.web.bind.annotation.GetMapping;
+ import org.springframework.web.bind.annotation.PathVariable;
  import org.springframework.web.bind.annotation.RequestMapping;
  import org.springframework.web.bind.annotation.RequestMethod;
  import org.springframework.web.bind.annotation.RequestParam;
@@ -7,13 +9,16 @@
  @org.springframework.web.bind.annotation.RestController
  @RequestMapping("/hello")
  public class RestController {
-     @RequestMapping(value = {"", "/", "home"})
-     public String sayHello() {
-         return "Hello From Bridgelabz..";
-     }
-
-     @RequestMapping(value = {"/query"}, method = RequestMethod.GET)
-     public String sayHello(@RequestParam(value= "name")String name){
-         return "Hello "+ name +" From Bridgelabz";
-     }
+ 	@RequestMapping(value = { "", "/", "/home" })
+ 	public String sayHello() {
+ 		return "Hello from BridgeLabz";
+ 	}
+ 	 @RequestMapping(value={"/query"}, method=RequestMethod.GET)
+ 	    public String sayHello(@RequestParam(value="name") String name) {
+ 	        return "Hello" + name + "From Bridgelabz";
+ 	    }
+ 	  @GetMapping("/param/{name}")
+ 	    public String sayHelloParam(@PathVariable String name) {
+ 	        return "Hello " + name + " From Bridgelabz";
+ 	    }
  }
