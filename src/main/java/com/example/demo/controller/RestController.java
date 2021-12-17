567c7e11279123ae655b407ddc36553daed8563b
ravi <ravi@gmail.com>
2021-12-17 22:35:35 +0530

    first name and last name using Postmethod in the body

--- src/main/java/com/example/demo/controller/RestController.java
+++ src/main/java/com/example/demo/controller/RestController.java
@@ -2,9 +2,13 @@
 
  import org.springframework.web.bind.annotation.GetMapping;
  import org.springframework.web.bind.annotation.PathVariable;
+ import org.springframework.web.bind.annotation.PostMapping;
+ import org.springframework.web.bind.annotation.RequestBody;
  import org.springframework.web.bind.annotation.RequestMapping;
  import org.springframework.web.bind.annotation.RequestMethod;
  import org.springframework.web.bind.annotation.RequestParam;
+
+ import com.example.demo.model.User;
 
  @org.springframework.web.bind.annotation.RestController
  @RequestMapping("/hello")
@@ -20,5 +24,10 @@
  	  @GetMapping("/param/{name}")
  	    public String sayHelloParam(@PathVariable String name) {
  	        return "Hello " + name + " From Bridgelabz";
+ 	    }
+
+ 	    @PostMapping("/post")
+ 	    public String sayHello(@RequestBody User user) {
+ 	        return "Hello " + user.getFirstName() + " " + user.getLastName() + " From Bridgelabz";
  	    }
  }
